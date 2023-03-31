5056ea3c48ac53ed00d61134c2e475fbbb47139d
Leehaney gee <georgeleehaney20@gmail.com>
2023-03-31 16:03:45 +0300

    Feat: Validate name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id
   has_many :likes
 
+  validates :name, presence: true
+
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
