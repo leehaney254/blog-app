686454cdba44ade62943e353d772e40fd8aeab59
Leehaney gee <georgeleehaney20@gmail.com>
2023-03-31 11:21:41 +0300

    Feat: Add association to comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
-  belongs_to :users
-  belongs_to :posts
+  belongs_to :user
+  belongs_to :post
+
+  def self.update_comments_counter(post_id)
+    post = Post.find(post_id)
+    post.update(comments_counter: post.comments.count)
+  end
 end
