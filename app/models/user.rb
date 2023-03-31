ad02c16fa61364cbbec136267e3db0db7ec63643
Leehaney gee <georgeleehaney20@gmail.com>
2023-03-31 11:22:52 +0300

    Feat: Add association to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   has_many :comments
-  has_many :posts
+  has_many :posts, foreign_key: :author_id
   has_many :likes
+
+  def recent_posts
+    self.posts.order(created_at: :desc).limit(3)
+  end
 end
