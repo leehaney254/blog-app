d59edf6d4eb813c127f422e5559a1451062f013b
Leehaney gee <georgeleehaney20@gmail.com>
2023-03-31 11:22:24 +0300

    Feat: Add association to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,14 @@
 class Post < ApplicationRecord
   has_many :comments
   has_many :likes
-  belongs_to :users
+  belongs_to :author, class_name: 'User'
+
+  def self.update_post_counter(user_id)
+    user = User.find(user_id)
+    user.update(posts_counter: user.posts.count)
+  end
+
+  def recent_comments
+    self.comments.order(created_at: :desc).limit(5)
+  end
 end
