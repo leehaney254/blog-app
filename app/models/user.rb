d4d67889b1af59b0e56d94b3af83fbaa45075007
Leehaney gee <georgeleehaney20@gmail.com>
2023-03-31 16:11:19 +0300

    Fet: validate postscounter

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :likes
 
   validates :name, presence: true
+  validates :PostsCounter, numericality: {only_integer: true, greater_than_or_equal_to: 0}
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
