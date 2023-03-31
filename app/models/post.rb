7d4e8d3a0088e090ab076374a5754aea6c856314
Leehaney gee <georgeleehaney20@gmail.com>
2023-03-31 16:17:19 +0300

    Feat: Add Post model validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,9 @@ class Post < ApplicationRecord
 
   after_save :update_post_counter
 
+  validates :Title, presence: true, length: {maximum: 250}
+  validates :CommentsCounter, :LikesCounter, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
