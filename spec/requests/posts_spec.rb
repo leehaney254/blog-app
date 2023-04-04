45222ee21820c3dd3e3d681d40d09d1b9c7d29a7
Leehaney gee <georgeleehaney20@gmail.com>
2023-04-04 17:23:08 +0300

    Feat: Add post controller test

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+#we group together our users tests
+RSpec.describe "/posts", type: :request do
+
+  context "GET /index" do
+    it "renders a successful response" do
+      get user_posts_path(5)
+      expect(response).to be_successful
+    end
+
+    it "renders the correct template" do
+      get user_posts_path(5)
+      expect(response).to render_template("index")
+    end
+
+    it "includes correct placeholder text" do
+      get user_posts_path(5)
+      expect(response.body).to include("<h1>Users Posts</h1>")
+    end
+  end
+
+  context "GET /show" do
+    it "renders a successful response" do
+      get user_post_url(5, 7)
+      expect(response).to be_successful
+    end
+
+    it "renders the correct template" do
+      get user_post_url(5, 7)
+      expect(response).to render_template("show")
+    end
+
+    it "includes correct placeholder text" do
+      get user_post_url(5, 7)
+      expect(response.body).to include("<h1>Show post</h1>")
+    end
+  end
+end
