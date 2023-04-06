122e852364a3b68daa7f9b91fe3a2c269c5a8c6f
Leehaney gee <georgeleehaney20@gmail.com>
2023-04-06 12:10:45 +0300

    Feat: Only display recent posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = User.find(params[:id]).posts
+    @posts = @user.recent_posts
   end
 end
