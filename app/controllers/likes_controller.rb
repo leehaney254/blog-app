49f43dfc899301499b83c71f8687ae48dc918af7
Leehaney gee <georgeleehaney20@gmail.com>
2023-04-07 10:02:20 +0300

    Feat: Add likes controller

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,13 @@
+class LikesController < ApplicationController
+  def create
+    @post = params[:post_id]
+    @user = current_user
+    @like =  @user.likes.new(post_id: @post)
+
+    if @like.save
+      redirect_to user_post_path(@user.id, @post), success: "Liked successfully."
+    else
+      redirect_to user_post_path(@user.id, @post), error: "Sorry something went wrong"
+    end
+  end
+end
