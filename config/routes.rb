06eb3fab4dd61b1451c1cb4b612c77f575600bca
Leehaney gee <georgeleehaney20@gmail.com>
2023-04-07 09:15:22 +0300

    Feat: Add comments routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # root "articles#index"
   root 'users#index'
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show new create]
+    resources :posts, only: %i[index show new create] do
+      resources :comments, only: %i[new create]
+    end
   end
 end
