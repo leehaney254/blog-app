fa9381caa2b8441423e185e021b4fea256a21d41
Leehaney gee <georgeleehaney20@gmail.com>
2023-04-07 05:09:26 +0300

    Feat: Add new and create method to controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,12 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @comments = @post.comments
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+
+  end
 end
