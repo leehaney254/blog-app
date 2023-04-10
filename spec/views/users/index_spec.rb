ce3d0c28b603ef842a7904896cb10fc76e6882d9
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-04-10 19:06:59 +0300

    Add users index view test

---
+++ spec/views/users/index_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature 'Users', type: :feature do
+  before do
+    @user1 = User.create(name: 'user1', posts_counter: 3,
+                         photo: 'https://unsplash.com/photos/F_-0BxGuVvo')
+    @user2 = User.create(name: 'user2', posts_counter: 2,
+                         photo: 'https://unsplash.com/photos/F_-0BxGuVvo')
+    @user3 = User.create(name: 'user3', posts_counter: 1,
+                         photo: 'https://unsplash.com/photos/F_-0BxGuVvo')
+  end
+
+  scenario 'User visits the index page' do
+    visit users_path
+
+    expect(page).to have_content('user1')
+    expect(page).to have_content('user2')
+    expect(page).to have_content('user3')
+
+    expect(page).to have_selector('.user-photo', count: 3)
+
+    expect(page).to have_content(@user1.posts_counter)
+    expect(page).to have_content(@user2.posts_counter)
+    expect(page).to have_content(@user3.posts_counter)
+
+    expect(page).to have_link('user1', href: user_path(@user1))
+    expect(page).to have_link('user2', href: user_path(@user2))
+    expect(page).to have_link('user3', href: user_path(@user3))
+  end
+end
