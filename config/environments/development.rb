4b169eb132560648ece7fba5eba05750e57ef164
Leehaney gee <georgeleehaney20@gmail.com>
2023-04-12 19:57:04 +0300

    Feat: Add email confirmation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,20 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: '587',
+    domain: 'gmail.com',
+    user_name: 'blogapptest307@gmail.com',
+    password: 'knqqqyelxcfjxgsg',
+    authentication: 'plain',
+    :tsl => true,
+    enable_starttls_auto: true
+  }
 
   config.action_mailer.perform_caching = false
 
@@ -68,15 +82,4 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    domain: 'example.com',
-    user_name: 'blogapptest307@gmail.com',
-    password: 'Leonileo1!',
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
 end
