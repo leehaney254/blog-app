ac61d4e9f1647be45a945de85f492fd55b11806c
Leehaney gee <georgeleehaney20@gmail.com>
2023-04-13 14:09:24 +0300

    Feat: Add delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -20,5 +20,18 @@ class CommentsController < ApplicationController
       flash.now[:error] = 'Sorry something went wrong'
       render :new
     end
+  end 
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    if @comment.destroy
+      flash[:notice] = "Friend deleted successfully!"
+    else
+      flash[:alert] = "Failed to delete friend"
+    end
+
+    redirect_to user_post_path(@user, @post)
   end
 end
