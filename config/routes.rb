3f05710a97f95a35c3f1c7c0bbb7997713b1eff2
Leehaney gee <georgeleehaney20@gmail.com>
2023-04-15 11:05:31 +0300

    Feat: Add api routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,13 @@ Rails.application.routes.draw do
       resources :likes, only: %i[create]
     end
   end
+
+  namespace :api do
+    post 'login', to: 'login#index'
+    resources :users, only: [:index] do
+      resources :posts, only: [:index] do
+        resources :comments, only: [:index, :create]
+      end
+    end
+  end
 end
